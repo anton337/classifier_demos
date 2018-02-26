1734051023c12715d1dfade4e0e789ddf580ffed
Anton Kodochygov <bricks13@hotmail.com>
2018-02-25 18:01:28 -0600

    ...

--- include/diagnostics/CNNActivationProbe.h
+++ include/diagnostics/CNNActivationProbe.h
@@ -5,6 +5,8 @@ template<typename T>
 struct CNNActivationProbe
 {
 
+    LayerType layer_type;
+
     long num_inputs;
     long  input_grid_nx;
     long  input_grid_ny;
@@ -16,20 +18,93 @@ struct CNNActivationProbe
     T * input_dat;
     T * output_dat;
 
+    long num_kernel;
+    long num_kernel_nx;
+    long num_kernel_ny;
+
+    long num_output_conv;
+    long num_output_conv_nx;
+    long num_output_conv_ny;
+
+    T * kernel_dat;
+    T * output_conv_dat;
+
     CNNActivationProbe(ConvolutionalNN<T> * cnn,long layer)
     {
-         input_dat = NULL;
-        output_dat = NULL;
-        if(input_dat == NULL)
+
+        layer_type = cnn -> n_layer_type[layer];
+
+        switch ( layer_type )
         {
-            input_dat = new T[perceptron->n_nodes[layer+1]*perceptron->n_nodes[layer]];
+          case RELU_LAYER :
+            {
+              kernel_dat = NULL;
+              num_kernel = 0;
+              long M = n_features[layer];
+              long N = n_features[layer+1];
+              long dx = nx[layer];
+              long dy = ny[layer];
+              output_conv_dat = new T[M*N*dx*dy];
+              break;
+            }
+
+          case FULLY_CONNECTED_LAYER :
+            {
+               input_dat = NULL;
+              output_dat = NULL;
+              if(input_dat == NULL)
+              {
+                  input_dat = new T[perceptron->n_nodes[layer+1]*perceptron->n_nodes[layer]];
+              }
+              num_inputs = perceptron->n_nodes[layer];
+              if(output_dat == NULL)
+              {
+                  output_dat = new T[perceptron->n_nodes[layer+1]];
+              }
+              num_outputs = perceptron->n_nodes[layer+1];
+              break;
+            }
+
+          case CONVOLUTIONAL_LAYER :
+            {
+              long M = n_features[layer];
+              long N = n_features[layer+1];
+              long wx = (kx[layer]/2);
+              long wy = (ky[layer]/2);
+              long dx = nx[layer] - wx*2;
+              long dy = ny[layer] - wy*2;
+              kernel_dat = NULL;
+              output_conv_dat = NULL;
+              if(kernel_dat == NULL)
+              {
+                  kernel_dat = new T[N*M*kx[layer]*ky[layer]];
+              }
+              num_kernel = N*M*kx[layer]*ky[layer];
+              if(output_conv_dat == NULL)
+              {
+                  output_conv_dat = new T[N*M*(dx)*(dy)];
+              }
+              num_output_conv = N*M*(dx)*(dy);
+              break;
+            }
+          case MAX_POOLING_LAYER :
+          case MEAN_POOLING_LAYER :
+            {
+              kernel_dat = NULL;
+              num_kernel = 0;
+              long M = n_features[layer];
+              long N = n_features[layer+1];
+              long dx = nx[layer] / pooling_factorx[layer+1];
+              long dy = ny[layer] / pooling_factory[layer+1];
+              output_conv_dat = new T[M*N*dx*dy];
+              break;
+            }
+
+          default :
+            {
+              break;
+            }
         }
-        num_inputs = perceptron->n_nodes[layer];
-        if(output_dat == NULL)
-        {
-            output_dat = new T[perceptron->n_nodes[layer+1]];
-        }
-        num_outputs = perceptron->n_nodes[layer+1];
     }
 
     void set_input_grid(int nx,int ny)
--- include/visualization/visualize_cnn.h
+++ include/visualization/visualize_cnn.h
@@ -2,7 +2,93 @@
 #define VISUALIZE_CNN_H
 
 #include "ConvolutionalNN.h"
-#include 
+#include "CNNActivationProbe.h"
+#include "display.h"
+
+template<typename T>
+struct VisualizeConvolutionalNeuralNetwork : public Display < T >
+{
+
+    ConvolutionalNeuralNetwork < double > * probe_convolutional_neural_network;
+    
+    void visualize_activation_probe ( Perceptron<T> * perceptron
+                                    , ActivationProbe<T> * activation_probe
+                                    , long in_nx 
+                                    , long out_nx 
+                                    , long in_ny 
+                                    , long out_ny 
+                                    , T min_x 
+                                    , T max_x 
+                                    , T min_y 
+                                    , T max_y 
+                                    )
+    {
+    
+        if(perceptron == NULL)
+        {
+            std::cout << "Perceptron is NULL" << std::endl;
+            return;
+        }
+    
+        if(activation_probe == NULL)
+        {
+            std::cout << "Activation probe is NULL" << std::endl;
+            return;
+        }
+    
+        activation_probe -> get_neuron_inputs ( perceptron , 0 );
+
+        activation_probe -> get_neuron_outputs ( perceptron , 0 );
+    
+        activation_probe -> set_input_grid ( in_nx , in_ny );
+    
+        activation_probe -> set_output_grid ( out_nx , out_ny );
+        
+        float val;
+        float val2;
+        T  in_dx = 0.9 / activation_probe ->  input_grid_nx;
+        T  in_dy = 0.9 / activation_probe ->  input_grid_ny;
+        T out_dx = 1.0 / activation_probe -> output_grid_nx;
+        T out_dy = 1.0 / activation_probe -> output_grid_ny;
+
+        glBegin(GL_QUADS);
+        for(long oy=0,k=0,o=0;oy<activation_probe->output_grid_ny;oy++)
+        {
+            for(long ox=0;ox<activation_probe->output_grid_nx;ox++,o++)
+            {
+                for(long iy=0;iy<activation_probe->input_grid_ny;iy++)
+                {
+                    for(long ix=0;ix<activation_probe->input_grid_nx;ix++,k++)
+                    {
+                        val  = activation_probe ->  input_dat [ k ] ;
+                        val2 = activation_probe -> output_dat [ o ] ;
+                        val += 0.5*val2;
+                        glColor3f(val,val,val);
+                        glVertex3f( min_x + (max_x-min_x)*(out_dx * ox + out_dx * in_dx* ix     )
+                                  , min_y + (max_y-min_y)*(out_dy * oy + out_dy * in_dy* iy     )
+                                  , 0
+                                  );
+                        glVertex3f( min_x + (max_x-min_x)*(out_dx * ox + out_dx * in_dx*(ix+1)  )
+                                  , min_y + (max_y-min_y)*(out_dy * oy + out_dy * in_dy* iy     )
+                                  , 0
+                                  );
+                        glVertex3f( min_x + (max_x-min_x)*(out_dx * ox + out_dx * in_dx*(ix+1)  )
+                                  , min_y + (max_y-min_y)*(out_dy * oy + out_dy * in_dy*(iy+1)  )
+                                  , 0
+                                  );
+                        glVertex3f( min_x + (max_x-min_x)*(out_dx * ox + out_dx * in_dx* ix     )
+                                  , min_y + (max_y-min_y)*(out_dy * oy + out_dy * in_dy*(iy+1)  )
+                                  , 0
+                                  );
+                    }
+                }
+            }
+        }
+        glEnd();
+    
+    }
+
+};
 
 #endif
 
