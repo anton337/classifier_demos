ce297859f11901454052f2866fd04026a52b10c0
Anton Kodochygov <bricks13@hotmail.com>
2018-05-26 18:36:18 -0500

    verifying digit learning is still functional

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
-export PROJECT=spiral_fitting
-#export PROJECT=simple_ann_classification
+#export PROJECT=spiral_fitting
+export PROJECT=simple_ann_classification
 #export PROJECT=xor_tutorial
 #export PROJECT=signal_tracking
 #export PROJECT=rbm_single_layer_example
